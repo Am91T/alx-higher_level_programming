9871d9363501f1f2053b53699c7f8ba990c25d1e
“Am91T” <tiouk.amine@gmail.com>
2024-01-04 14:33:50 -0800

    done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,15 +6,38 @@ class Rectangle():
     """this class defines a square with a private size attribute."""
 
     def __init__(self, width=0, height=0):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle. Defaults to 0.
+            height (int): The height of the rectangle. Defaults to 0.
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """
+        Getter method for retrieving the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Setter method for setting the width of the rectangle.
+
+        Args:
+            value (int): The new width value.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -24,10 +47,26 @@ class Rectangle():
 
     @property
     def height(self):
+        """
+        Getter method for retrieving the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Setter method for setting the height of the rectangle.
+
+        Args:
+            value (int): The new height value.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
