aa524c6e158ad752cec8b53b8a9e8a367a6d4460
“Am91T” <tiouk.amine@gmail.com>
2024-01-09 13:05:46 -0800

    done

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,9 @@
+#!/usr/bin/python3
+'''
+Module for JSON serialization
+'''
+
+
 def class_to_json(obj):
-    """Converts a class instance to a JSON-serializable dictionary."""
-    result = {}
-
-    # Iterate over the attributes of the object
-    for attr_name, attr_value in obj.__dict__.items():
-        # Check if the attribute is a simple data type
-        if isinstance(attr_value, (int, str, bool, list, dict)):
-            result[attr_name] = attr_value
-
-    return result
+    ''' Returns dictionary description with data list '''
+    return obj.__dict__
