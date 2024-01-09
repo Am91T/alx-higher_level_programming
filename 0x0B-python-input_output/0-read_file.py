f552cbdc1567f4c2eb497925595386a38c34829a
“Am91T” <tiouk.amine@gmail.com>
2024-01-09 08:15:58 -0800

    Done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Module to read the content of the file"""
 
+
 def read_file(filename=""):
     """
     Read the contents of a text file and print it to the console.
 
     Args:
-        filename (str): The name of the text file to be read. Default is an empty string.
+        filename (str): The name of the text file to be read.
+        Default is an empty string.
 
     Returns:
         None
