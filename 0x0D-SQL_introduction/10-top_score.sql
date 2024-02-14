b5cf9b599253b4f84cb6cc24ce4a04c688eb237e
“Am91T” <tiouk.amine@gmail.com>
2024-02-14 23:29:59 +0100

    Done

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
--- Script that lists all records of a table
-SELECT score, name FROM second_table ORDER BY name;
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
