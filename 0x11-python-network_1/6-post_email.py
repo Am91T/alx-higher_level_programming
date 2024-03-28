9261d33ff5704dc2a10f71422c0749ec1a64aaa9
“Am91T” <tiouk.amine@gmail.com>
2024-03-28 17:43:37 +0000

    Done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
-"""
-import sys
-import urllib.request
-
+'''
+send POST request to custom URL with email as a parameter
+'''
+import requests
+from sys import argv
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    response = requests.post(argv[1], data={'email': argv[2]})
+    print(response.text)
